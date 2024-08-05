d36c3dac45096029be8af4732ca36a423570ccf6
Suppe <Suppe04@gmail.com>
2024-08-05 16:08:08 +0200

    Removed Unused Code

--- src/BasePatternComponents/CampaignEndComponent.tsx
+++ src/BasePatternComponents/CampaignEndComponent.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-
-
 // Creating the frontpage demanding a boolean parameter of a button click 
 export default function CampaignEnd() {
 
--- src/BasePatternComponents/CampaignStartComponent.tsx
+++ src/BasePatternComponents/CampaignStartComponent.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-
 // Creating the frontpage demanding a boolean parameter of a button click 
 export default function CampaignStart(){
 
