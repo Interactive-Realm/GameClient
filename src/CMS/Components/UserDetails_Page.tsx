9c766beb16093b576a18d11e937dcd49be9499e1
Suppe <Suppe04@gmail.com>
2024-09-16 15:01:08 +0200

    User Details Page Features

--- src/CMS/Components/UserDetails_Page.tsx
+++ src/CMS/Components/UserDetails_Page.tsx
@@ -1,4 +1,4 @@
-import {  Row, Col, Badge, Dropdown, ListGroup, Card  } from 'react-bootstrap';
+import {  Row, Col, Badge, Dropdown, ListGroup, Card, Table  } from 'react-bootstrap';
 import { Chart } from "react-google-charts";
 
 export const data = [
@@ -35,63 +35,53 @@ const UserDetails_Page = () => {
                 </Badge>                                            
             </Row>
             <Row sm={12} className="justify-content-center align-items-center mx-auto g-0">
-                <ListGroup horizontal className="text-center">
-                    <ListGroup.Item className="flex-fill"><Col>
-                    <h1 className="display-6">100000</h1>
-                    Visits
-                    </Col></ListGroup.Item>
-                    <ListGroup.Item className="justify-content-center flex-fill"><Col>
-                    <h1 className="display-6">100000</h1>
-                    Unique Users
-                    </Col></ListGroup.Item>
-                    <ListGroup.Item className="justify-content-center flex-fill"><Col>
-                    <h1 className="display-6">100000</h1>
-                    Plays
-                    </Col></ListGroup.Item>
-                    <ListGroup.Item className="justify-content-center flex-fill"><Col>
-                    <h1 className="display-6">100000</h1>
-                    Playthroughs
-                    </Col></ListGroup.Item>
-                </ListGroup>
+                <Card body>
+                <Table hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>User ID</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Gender</th>                        
+                            <th>Device</th>
+                            <th>Location</th>
+                            <th>E-Mail</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>3</td>
+                            <td>Male</td>
+                            <td>Android</td>
+                            <td>Lars Tyndskyndsmark</td>
+                            <td>Lars@Lars.Larsnet</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>3</td>
+                            <td>Male</td>
+                            <td>Android</td>
+                            <td>Lars Tyndskyndsmark</td>
+                            <td>Lars@Lars.Larsnet</td>
+                        </tr>                        
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>3</td>
+                            <td>Male</td>
+                            <td>Android</td>
+                            <td>Lars Tyndskyndsmark</td>
+                            <td>Lars@Lars.Larsnet</td>
+                        </tr>
+                    </tbody>
+                </Table>
+                </Card>
+                
             </Row>
-            
-        </Row>
-        
-        {/* Chart */}
-        <Row sm={12} className="g-0 gap-2">
-        
-            {/* Menu Line */}
-            <Row md={6} className="gap-1">
-                Traffic for Period
-                <Badge as={Dropdown} pill bg="secondary">
-                    10th of August
-                </Badge>                                            
-            </Row>
-        
-            {/* Number Jumbotron */}
-            <Row sm={12} className="justify-content-center align-items-center mx-auto g-0">
-                <Col sm={6}><Card body>
-                    <Chart
-                    chartType="PieChart"
-                    data={data}
-                    options={options}
-                    width={"100%"}
-                    height={"400px"}
-                    />
-                </Card></Col>
-                <Col sm={6}><Card body>
-                    <Chart
-                    chartType="PieChart"
-                    data={data}
-                    options={options}
-                    width={"100%"}
-                    height={"400px"}
-                    />
-                </Card></Col>
-
-        
-            </Row>
-        
+                    
         </Row>
         
         </Row>
