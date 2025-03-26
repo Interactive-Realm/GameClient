ed34b702c51d5bd59645898d7e0837f6c3fc015e
Suppe <Suppe04@gmail.com>
2025-03-26 15:07:06 +0100

    Removed last supabase files + folder

--- src/Supabase/index.ts
+++
@@ -1,5 +0,0 @@
-
-// User types (Different variations of user data)
-// THIS NEEDS TO BE COMBINED INTO ONE USERTYPE WITH FIELDS NOT BEING REQUIRED TO BE FILLED (EG. Score?)
-export * as UserTypes from './usertypes';
-
