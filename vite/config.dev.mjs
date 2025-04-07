2f9c376feb0dcf387183f04351b1e0293b0f8fcf
Christian <CStochholm@kami-io.com>
2025-04-07 13:49:41 +0200

    ShadCN/UI Installation

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import EnvironmentPlugin from 'vite-plugin-environment';
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
     plugins: [
-        react()
+        react(), tailwindcss()
     ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "..", "src")
+      },
+    },    
     server: {
         port: 8080
     }
--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 const buildmsg = () => {
     return {
@@ -21,8 +23,14 @@ export default defineConfig({
     base: './',
     plugins: [
         react(),
-        buildmsg()
+        buildmsg(), 
+        tailwindcss()
     ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "..", "src")
+      },
+    },    
     logLevel: 'warning',
     build: {
         rollupOptions: {
